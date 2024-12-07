628c5f181e9a1f25f475cfae55a7b48ffca3f6ac
ImZeera <160336922+ImZeera@users.noreply.github.com>
2024-12-07 23:18:39 +0700

    Dashboard Dosen

--- DashboardDosen.js
+++ DashboardDosen.js
@@ -1,108 +1,139 @@
-import React from "react";
-import { View, Text, ImageBackground, ScrollView, Image, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView, Image, TouchableOpacity, ActivityIndicator, StyleSheet } from "react-native";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebaseConfig";
 
-const DashboardDosen = ({ navigation }) => {
+const NotificationItem = ({ type, dosen, reviewer }) => {
+  const isJanjiTemu = type === "Janji Temu";
   return (
-    <View style={styles.container2}>
-      <ScrollView style={styles.scrollContent}>
-
-        <View style={styles.menuContainer}>
-          <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("Bimbingan Dosen")}>
-            <Image source={require("./assets/OnlineLecture.png")} style={styles.menuIcon} />
-            <Text style={styles.menuText}>Bimbingan</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("Janji Temu Dosen")}>
-            <Image source={require("./assets/Kalendar.png")} style={styles.menuIcon} />
-            <Text style={styles.menuText}>Janji Temu</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("Review Dosen")}>
-            <Image source={require("./assets/AddDokumen.png")} style={styles.menuIcon} />
-            <Text style={styles.menuText}>Review</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.scrollView}>
-          <View style={styles.notificationContainerTitle}>
-            <View style={styles.textContainer}>
-              <Text style={styles.notificationTitle}>Tugas Baru Masuk</Text>
-              <Text style={styles.notificationTitle2}>Dari Mahasiswa: Fakhri</Text>
-            </View>
-            <Image source={require("./assets/IkonNotif.png")} style={styles.menuIconNotif} />
-          </View>
-          <View style={styles.notificationContainer}>
-            <View style={styles.notificationMessage}>
-              <Text style={styles.notificationText}>“Tugas ini harus diselesaikan dalam 3 hari.”</Text>
-            </View>
-          </View>
-
-          <View style={styles.notificationContainerTitle}>
-            <View>
-              <Text style={styles.notificationTitle}>Janji Temu Dosen</Text>
-              <Text style={styles.notificationTitle2}>Dengan Mahasiswa: Joko</Text>
-            </View>
-            <Image source={require("./assets/IkonNotif2.png")} style={styles.menuIconNotif} />
-          </View>
-          <View style={styles.notificationContainer}>
-            <View style={styles.notificationMessage}>
-              <Text style={styles.notificationText}>26 April 2024</Text>
-              <Text style={styles.notificationText}>At 11:00 - 12:00 AM</Text>
-            </View>
-          </View>
-
-          <View style={styles.notificationContainerTitle}>
-            <View>
-              <Text style={styles.notificationTitle}>Review Tugas Masuk</Text>
-              <Text style={styles.notificationTitle2}>Dari Mahasiswa: Fakhri</Text>
-            </View>
-            <Image source={require("./assets/IkonNotif.png")} style={styles.menuIconNotif} />
-          </View>
-          <View style={styles.notificationContainer}>
-            <View style={styles.notificationMessage}>
-              <Text style={styles.notificationText}>“Silakan review tugas dan berikan feedback.”</Text>
-            </View>
-          </View>
-        </View>
-      </ScrollView>
-
-      <View style={styles.bottomMenuContainer}>
-        <TouchableOpacity style={styles.bottomMenuItem}>
-          <Image source={{ uri: "https://img.icons8.com/ios-filled/50/000000/home.png" }} style={styles.bottomMenuIcon} />
-          <Text style={styles.bottomMenuText}>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomMenuItem}>
-          <Image source={{ uri: "https://img.icons8.com/ios-filled/50/000000/compass.png" }} style={styles.bottomMenuIcon} />
-          <Text style={styles.bottomMenuText}>Lainnya</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomMenuItem}>
-          <Image source={{ uri: "https://img.icons8.com/ios-filled/50/000000/graduation-cap.png" }} style={styles.bottomMenuIcon} />
-          <Text style={styles.bottomMenuText}>Akademik</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomMenuItem}>
-          <Image source={{ uri: "https://img.icons8.com/ios-filled/50/000000/secured-letter.png" }} style={styles.bottomMenuIcon} />
-          <Text style={styles.bottomMenuText}>Pesan</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomMenuItem}>
-          <Image source={{ uri: "https://img.icons8.com/ios-filled/50/000000/user.png" }} style={styles.bottomMenuIcon} />
-          <Text style={styles.bottomMenuText}>Akun</Text>
-        </TouchableOpacity>
+    <View style={styles.notificationContainerTitle}>
+      <View>
+        <Text style={styles.notificationTitle}>{isJanjiTemu ? "Ada Janji Temu Nih" : "Ajuan review"}</Text>
+        <Text style={styles.notificationTitle2}>{isJanjiTemu ? `Dengan ${dosen}` : `Dari ${reviewer}`}</Text>
       </View>
+      <Image source={isJanjiTemu ? require("./assets/IkonNotif2.png") : require("./assets/IkonNotif.png")} style={styles.menuIconNotif} />
     </View>
   );
 };
 
+const DashboardDosen = ({ navigation }) => {
+  const [notifications, setNotifications] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchNotifications = async () => {
+    try {
+      const janjiTemuSnapshot = await getDocs(collection(db, "janjiTemu"));
+      const janjiTemuData = janjiTemuSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        type: "Janji Temu",
+        ...doc.data(),
+      }));
+
+      const reviewsSnapshot = await getDocs(collection(db, "reviews"));
+      const reviewsData = reviewsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        type: "Review",
+        ...doc.data(),
+      }));
+
+      const allNotifications = [...janjiTemuData, ...reviewsData];
+      setNotifications(allNotifications);
+    } catch (error) {
+      console.error("Error fetching notifications: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.mainContainer}>
+      <ScrollView>
+        <View style={styles.menuContainer}>
+          <MenuItem navigation={navigation} routeName="Janji Temu Dosen" icon={require("./assets/Kalendar.png")} label="Janji Temu" />
+          <MenuItem navigation={navigation} routeName="Review Dosen" icon={require("./assets/AddDokumen.png")} label="Review" />
+          <MenuItem navigation={navigation} routeName="Riwayat Dosen" icon={require("./assets/IkonHistory.png")} label="Riwayat" />
+        </View>
+
+        <ScrollView style={styles.notificationScroll}>
+          {notifications.map((item) => (
+            <NotificationItem key={item.id} type={item.type} dosen={item.mahasiswa} reviewer={item.mahasiswa} />
+          ))}
+        </ScrollView>
+      </ScrollView>
+
+      <BottomNavigation navigation={navigation} />
+    </View>
+  );
+};
+
+const MenuItem = ({ navigation, routeName, icon, label }) => (
+  <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate(routeName)}>
+    <Image source={icon} style={styles.menuIcon} />
+    <Text style={styles.menuText}>{label}</Text>
+  </TouchableOpacity>
+);
+
+const BottomNavigation = ({ navigation }) => (
+  <View style={styles.bottomMenuContainer}>
+    <TouchableOpacity style={styles.bottomMenuItem}>
+      <Image source={{ uri: "https://img.icons8.com/ios-filled/50/000000/home.png" }} style={styles.activebottomMenuIcon} />
+      <Text style={styles.activebottomMenuText}>Home</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.bottomMenuItem} onPress={() => navigation.navigate("Janji Temu Dosen")}>
+      <Image source={{ uri: "https://img.icons8.com/?size=100&id=116296&format=png&color=000000" }} style={styles.bottomMenuIcon} />
+      <Text style={styles.bottomMenuText}>Janji Temu</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.bottomMenuItem} onPress={() => navigation.navigate("Review Dosen")}>
+      <Image source={{ uri: "https://img.icons8.com/?size=100&id=99292&format=png&color=000000" }} style={styles.bottomMenuIcon} />
+      <Text style={styles.bottomMenuText}>Review</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.bottomMenuItem} onPress={() => navigation.navigate("Riwayat Dosen")}>
+      <Image source={{ uri: "https://img.icons8.com/?size=100&id=10058&format=png&color=000000" }} style={styles.bottomMenuIcon} />
+      <Text style={styles.bottomMenuText}>Riwayat</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.bottomMenuItem} onPress={() => navigation.navigate("AkunDosen")}>
+      <Image source={{ uri: "https://img.icons8.com/ios-filled/50/000000/user.png" }} style={styles.bottomMenuIcon} />
+      <Text style={styles.bottomMenuText}>Akun</Text>
+    </TouchableOpacity>
+  </View>
+);
+
 const styles = StyleSheet.create({
-  container2: {
+  mainContainer: {
     flex: 1,
-    paddingHorizontal: 20,
-    paddingTop: 10,
-    backgroundColor: "#FFF",
+    backgroundColor: "#fff",
+  },
+  notificationScroll: {
+    maxHeight: 750,
+    marginTop: 10,
+  },
+  rowContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  notificationIcon: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
   },
   menuContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     marginVertical: 10,
+    marginHorizontal: 8,
+    paddingTop: 15,
   },
   menuItem: {
     backgroundColor: "#FFF",
@@ -116,6 +147,8 @@ const styles = StyleSheet.create({
     elevation: 6,
     shadowRadius: 15,
     shadowOffset: { width: 1, height: 13 },
+    marginLeft: 20,
+    marginRight: 20,
   },
   menuIcon: {
     width: 50,
@@ -127,31 +160,17 @@ const styles = StyleSheet.create({
     color: "#939393",
     fontWeight: "bold",
   },
-  scrollView: {
-    marginTop: 10,
-  },
   notificationContainerTitle: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     marginTop: 20,
-    backgroundColor: "#63ABE6",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    paddingVertical: 10,
+    backgroundColor: "#3470A2",
+    borderRadius: 20,
+    paddingVertical: 12,
     paddingHorizontal: 15,
-  },
-  notificationContainer: {
-    backgroundColor: "#fff",
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    paddingTop: 5,
-    paddingBottom: 5,
-    shadowColor: "black",
-    shadowOpacity: 0.8,
-    elevation: 6,
-    shadowRadius: 15,
-    shadowOffset: { width: 1, height: 13 },
+    marginLeft: 20,
+    marginRight: 20,
   },
   notificationTitle: {
     fontSize: 16,
@@ -161,14 +180,6 @@ const styles = StyleSheet.create({
   notificationTitle2: {
     fontSize: 12,
     color: "#FFF",
-  },
-  notificationMessage: {
-    padding: 5,
-  },
-  notificationText: {
-    fontSize: 14,
-    color: "#515151",
-    marginLeft: 10,
   },
   menuIconNotif: {
     width: 24,
@@ -195,10 +206,19 @@ const styles = StyleSheet.create({
   bottomMenuItem: {
     alignItems: "center",
   },
+  activebottomMenuIcon: {
+    width: 20,
+    height: 20,
+    tintColor: "#FFC727",
+  },
   bottomMenuIcon: {
     width: 20,
     height: 20,
     tintColor: "#8C8994",
+  },
+  activebottomMenuText: {
+    fontSize: 12,
+    color: "#FFC727",
   },
   bottomMenuText: {
     fontSize: 12,
