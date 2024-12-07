f2c2bd1f7a074b2706bec040c74b6e3ddbe57117
ImZeera <160336922+ImZeera@users.noreply.github.com>
2024-12-07 23:16:56 +0700

    App.js

--- App.js
+++ App.js
@@ -3,11 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CustomHeaderDosen from "./CustomHeaderDosen";
 import CustomHeaderMhs from "./CustomHeaderMhs";
-import DashboardAwal from "./DashboardAwal";
 import DashboardMhs from "./DashboardMhs";
 import DashboardDosen from "./DashboardDosen";
-import BimbinganMhs from "./BimbinganMhs";
-import BimbinganDosen from "./BimbinganDosen";
 import JanjiTemuMhs from "./JanjiTemuMhs";
 import JanjiTemuDosen from "./JanjiTemuDosen";
 import ReviewMhs from "./ReviewMhs";
@@ -16,41 +13,47 @@ import NotifikasiMhs from "./NotifikasiMhs";
 import NotifikasiDosen from "./NotifikasiDosen";
 import RiwayatMhs from "./RiwayatMhs";
 import RiwayatDosen from "./RiwayatDosen";
+import HLogin from "./HLogin";
+import HRegistrasi from "./HRegistrasi";
+import AkunMhs from "./AkunMhs";
+import AkunDosen from "./AkunDosen";
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SIKRISPI">
-        <Stack.Screen name="SIKRISPI" component={DashboardAwal} />
+      <Stack.Navigator initialRouteName="HLogin">
+        <Stack.Screen name="HLogin" component={HLogin} />
+        <Stack.Screen name="HRegistrasi" component={HRegistrasi} />
+        <Stack.Screen name="AkunMhs" component={AkunMhs} />
+        <Stack.Screen name="AkunDosen" component={AkunDosen} />
         <Stack.Screen
           name="Dashboard Mahasiswa"
           component={DashboardMhs}
           options={{
-            headerTitle: () => <CustomHeaderMhs />, // Ensure the correct header is used
+            headerTitle: () => <CustomHeaderMhs />,
           }}
         />
         <Stack.Screen
           name="Dashboard Dosen"
           component={DashboardDosen}
           options={{
-            headerTitle: () => <CustomHeaderDosen />, // Ensure the correct header is used
+            headerTitle: () => <CustomHeaderDosen />,
           }}
         />
-        <Stack.Screen name="Bimbingan Mahasiswa" component={BimbinganMhs} />
-        <Stack.Screen name="Bimbingan Dosen" component={BimbinganDosen} />
-        <Stack.Screen name="Janji Temu Mahasiswa" component={JanjiTemuMhs} />
-        <Stack.Screen name="Janji Temu Dosen" component={JanjiTemuDosen} />
-        <Stack.Screen name="Review Mahasiswa" component={ReviewMhs} />
-        <Stack.Screen name="Review Dosen" component={ReviewDosen} />
-        <Stack.Screen name="Notifikasi Mahasiswa" component={NotifikasiMhs} />
-        <Stack.Screen name="Notifikasi Dosen" component={NotifikasiDosen} />
-        <Stack.Screen name="Riwayat Mahasiswa" component={RiwayatMhs} />
-        <Stack.Screen name="Riwayat Dosen" component={RiwayatDosen} />
+        <Stack.Screen name="Janji Temu Mahasiswa" component={JanjiTemuMhs} options={{ headerTitle: "Janji Temu Mahasiswa" }} />
+        <Stack.Screen name="Janji Temu Dosen" component={JanjiTemuDosen} options={{ headerTitle: "Janji Temu Dosen" }} />
+
+        <Stack.Screen name="Review Mahasiswa" component={ReviewMhs} options={{ headerTitle: "Review Mahasiswa" }} />
+        <Stack.Screen name="Review Dosen" component={ReviewDosen} options={{ headerTitle: "Review Dosen" }} />
+
+        <Stack.Screen name="Notifikasi Mahasiswa" component={NotifikasiMhs} options={{ headerTitle: "Notifikasi Mahasiswa" }} />
+        <Stack.Screen name="Notifikasi Dosen" component={NotifikasiDosen} options={{ headerTitle: "Notifikasi Dosen" }} />
+
+        <Stack.Screen name="Riwayat Mahasiswa" component={RiwayatMhs} options={{ headerTitle: "Riwayat Mahasiswa" }} />
+        <Stack.Screen name="Riwayat Dosen" component={RiwayatDosen} options={{ headerTitle: "Riwayat Dosen" }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
