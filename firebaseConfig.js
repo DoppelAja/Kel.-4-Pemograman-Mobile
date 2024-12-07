40ac5f005125cd2dc48973511981559c7b38aa2b
ImZeera <160336922+ImZeera@users.noreply.github.com>
2024-12-07 23:19:10 +0700

    Firebase Config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyApLz3MNqPqsuRQnmq-OFrBnJOFV7W7AT0",
@@ -11,6 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
